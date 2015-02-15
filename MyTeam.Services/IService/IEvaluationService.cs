99d1567e8276f01428fee63bf985b4c58e42c3f4
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-15 23:17:44 +0000

    Added Evaluation service files.

---
+++ MyTeam.Services/IService/IEvaluationService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyTeam.Data;
+
+namespace MyTeam.Services.IService
+{
+    
+    interface IEvaluationService
+    {
+
+        // CREATE ===================================================================
+        // addEvaluation
+        void addEvaluation(Evaluation evaluation);
+
+        // READ =====================================================================
+        // getEvaluationes
+        IList<Evaluation> getEvaluationes();
+
+        // getEvaluation
+        Evaluation getEvaluation(int id);
+
+        // UPDATE ===================================================================
+        // editEvaluation
+        void editEvaluation(Evaluation evaluation);
+
+        // DELETE ===================================================================
+        // deleteEvaluation
+        void deleteEvaluation(Evaluation evaluation);
+
+    }
+
+}
---
+++ MyTeam.Services/Service/EvaluationService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyTeam.Services.IService;
+using MyTeam.Data;
+using MyTeam.Data.DAO;
+
+namespace MyTeam.Services.Service
+{
+   
+    public class EvaluationService : IEvaluationService
+    {
+
+        private EvaluationDAO _EvaluationDAO;
+        public EvaluationService()
+        {
+            _EvaluationDAO = new EvaluationDAO();
+        }
+
+        // CREATE ===================================================================
+        // addEvaluation
+        public void addEvaluation(Evaluation evaluation)
+        {
+            _EvaluationDAO.addEvaluation(evaluation);
+        }
+
+        // READ =====================================================================
+        // getEvaluationes
+        public IList<Evaluation> getEvaluationes()
+        {
+            return _EvaluationDAO.getEvaluations();
+        }
+
+        // getEvaluation
+        public Evaluation getEvaluation(int id)
+        {
+            return _EvaluationDAO.getEvaluation(id);
+        }
+
+        // UPDATE ===================================================================
+        // editEvaluation
+        public void editEvaluation(Evaluation evaluation)
+        {
+            _EvaluationDAO.editEvaluation(evaluation);
+        }
+
+        // DELETE ===================================================================
+        // deleteEvaluation
+        public void deleteEvaluation(Evaluation evaluation)
+        {
+            _EvaluationDAO.deleteEvaluation(evaluation);
+        }
+
+    }
+
+}
