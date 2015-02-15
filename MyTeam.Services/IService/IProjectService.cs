fc5483fb088f39a69cea1fa9926384fc1e7bfde4
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-15 23:20:49 +0000

    Added Project service layer.

---
+++ MyTeam.Services/IService/IProjectService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyTeam.Data;
+
+namespace MyTeam.Services.IService
+{
+    
+    interface IProjectService
+    {
+
+        // CREATE ===================================================================
+        // addProject
+        void addProject(Project project);
+
+        // READ =====================================================================
+        // getProjectes
+        IList<Project> getProjectes();
+
+        // getProject
+        Project getProject(int id);
+
+        // UPDATE ===================================================================
+        // editProject
+        void editProject(Project project);
+
+        // DELETE ===================================================================
+        // deleteProject
+        void deleteProject(Project project);
+
+    }
+
+}
---
+++ MyTeam.Services/Service/ProjectService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyTeam.Services.IService;
+using MyTeam.Data;
+using MyTeam.Data.DAO;
+
+namespace MyTeam.Services.Service
+{
+    
+    public class ProjectService : IProjectService
+    {
+
+        private ProjectDAO _ProjectDAO;
+        public ProjectService()
+        {
+            _ProjectDAO = new ProjectDAO();
+        }
+
+        // CREATE ===================================================================
+        // addProject
+        public void addProject(Project project)
+        {
+            _ProjectDAO.addProject(project);
+        }
+
+        // READ =====================================================================
+        // getProjectes
+        public IList<Project> getProjects()
+        {
+            return _ProjectDAO.getProjects();
+        }
+
+        // getProject
+        public Project getProject(int id)
+        {
+            return _ProjectDAO.getProject(id);
+        }
+
+        // UPDATE ===================================================================
+        // editProject
+        public void editProject(Project project)
+        {
+            _ProjectDAO.editProject(project);
+        }
+
+        // DELETE ===================================================================
+        // deleteProject
+        public void deleteProject(Project project)
+        {
+            _ProjectDAO.deleteProject(project);
+        }
+
+    }
+
+}
