d285f5d5df3b74861fe83ac850c339cc02186865
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-15 22:37:19 +0000

    Added WorkTask data access.

---
+++ MyTeam.Data/IDAO/IWorkTaskDAO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTeam.Data.IDAO
+{
+    
+    interface IWorkTaskDAO
+    {
+
+        // CREATE ====================================================================
+        // addWorkTask
+        void addWorkTask(WorkTask worktask);
+
+        // READ ======================================================================
+        // getWorkTasks
+        IList<WorkTask> getWorkTasks();
+
+        // getWorkTask
+        WorkTask getWorkTask(int id);
+
+        // UPDATE ====================================================================
+        // editWorkTask
+        void editWorkTask(WorkTask worktask);
+
+        // DELETE ====================================================================
+        // deleteWorkTask
+        void deleteWorkTask(WorkTask worktask);
+
+    }
+
+}
+
