1223c1afdb91f26152411d3a08b68592b13f63b8
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-15 23:33:40 +0000

    Added WorkTask service files.

---
+++ MyTeam.Services/IService/IWorkTaskService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyTeam.Data;
+
+namespace MyTeam.Services.IService
+{
+    
+    interface IWorkTaskService
+    {
+
+        // CREATE ===================================================================
+        // addWorkTask
+        void addWorkTask(WorkTask worktask);
+
+        // READ =====================================================================
+        // getWorkTaskes
+        IList<WorkTask> getWorkTaskes();
+
+        // getWorkTask
+        WorkTask getWorkTask(int id);
+
+        // UPDATE ===================================================================
+        // editWorkTask
+        void editWorkTask(WorkTask worktask);
+
+        // DELETE ===================================================================
+        // deleteWorkTask
+        void deleteWorkTask(WorkTask worktask);
+
+    }
+
+}
---
+++ MyTeam.Services/Service/WorkTaskService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyTeam.Services.IService;
+using MyTeam.Data;
+using MyTeam.Data.DAO;
+
+namespace MyTeam.Services.Service
+{
+    
+    public class WorkTaskService : IWorkTaskService
+    {
+        
+        private WorkTaskDAO _worktaskDAO;
+        public WorkTaskService()
+        {
+            _worktaskDAO = new WorkTaskDAO();
+        }
+
+        // CREATE ===================================================================
+        // addWorkTask
+        public void addWorkTask(WorkTask worktask)
+        {
+            _worktaskDAO.addWorkTask(worktask);
+        }
+
+        // READ =====================================================================
+        // getWorkTaskes
+        public IList<WorkTask> getWorkTaskes()
+        {
+            return _worktaskDAO.getWorkTasks();
+        }
+
+        // getWorkTask
+        public WorkTask getWorkTask(int id)
+        {
+            return _worktaskDAO.getWorkTask(id);
+        }
+
+        // UPDATE ===================================================================
+        // editWorkTask
+        public void editWorkTask(WorkTask worktask)
+        {
+            _worktaskDAO.editWorkTask(worktask);
+        }
+
+        // DELETE ===================================================================
+        // deleteWorkTask
+        public void deleteWorkTask(WorkTask worktask)
+        {
+            _worktaskDAO.deleteWorkTask(worktask);
+        }
+
+    }
+
+}
