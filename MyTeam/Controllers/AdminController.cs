956b30d3b445f08712ccc27cf75738744be7f506
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-21 23:56:08 +0000

    Added editRole action.

--- MyTeam/Controllers/AdminController.cs
+++ MyTeam/Controllers/AdminController.cs
@@ -46,21 +46,46 @@ namespace MyTeam.Controllers
         }
 
         // READ =====================================================================
-        // ControlPanel
+        // ControlPanel : Returns view that contains a list of Admin specific operations.
         public ActionResult ControlPanel() {
             return View();
         }
 
-        // Users
+        // Users : Returns list of all system registered users of any role.
         public ActionResult Users()
         {
             return View(_context.Users.ToList());
         }
 
-        // Roles
+        // Roles : Returns list of all system registered roles.
         public ActionResult Roles()
         {
             return View(_context.Roles.ToList());
+        }
+
+        // EditRole : Allows modification of the saved role name.
+        [HttpGet]
+        public ActionResult EditRole(string role)
+        {
+            var thisRole = _context.Roles.Where(r => r.Name.Equals(role, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            return View(thisRole);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditRole(Microsoft.AspNet.Identity.EntityFramework.IdentityRole role)
+        {
+            try
+            {
+                _context.Entry(role).State = System.Data.Entity.EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Roles", "Admin");
+            }
+            catch
+            {
+                return View();
+            }
+            
         }
 
         // UPDATE ===================================================================
