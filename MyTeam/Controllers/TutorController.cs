6cd5ebe1ec0b0960a79b0fcadd53c115cd7d9916
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-22 16:55:17 +0000

    Added Tutor controller.

---
+++ MyTeam/Controllers/TutorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyTeam.Models;
+
+namespace MyTeam.Controllers
+{
+    
+    public class TutorController : ApplicationController
+    {
+
+        // CREATE ===================================================================
+
+
+        // READ =====================================================================
+
+
+        // UPDATE ===================================================================
+
+
+        // DELETE ===================================================================
+    
+    }
+
+}
